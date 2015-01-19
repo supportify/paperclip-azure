bd0cb9bc2a673149f9c7ab0a78703fa2a2aa575c
Jordan Yaker <jordan.e.yaker@gmail.com>
2015-01-19 09:27:21 -0500

    Corrected issues with the file loading.

--- lib/paperclip-azure.rb
+++ lib/paperclip-azure.rb
@@ -1,3 +1,3 @@
-require 'azure/blob/blob_service'
-require 'azure/core/auth/shared_access_signature'
-require 'paperclip/storage/azure'
+require File.join(File.dirname(__FILE__), 'azure', 'blob', 'blob_service')
+require File.join(File.dirname(__FILE__), 'azure', 'core', 'auth', 'shared_access_signature')
+require File.join(File.dirname(__FILE__), 'paperclip', 'storage', 'azure')
