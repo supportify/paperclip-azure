1e884f43b296aa107524febdee5a12021602d168
Jordan Yaker <jordan.e.yaker@gmail.com>
2015-01-19 10:01:40 -0500

    Removed the require statement for the BlobService and brought the patch in to this file.

--- lib/paperclip-azure.rb
+++ lib/paperclip-azure.rb
@@ -1,3 +1,15 @@
-require File.join(File.dirname(__FILE__), 'azure', 'blob', 'blob_service')
+require 'azure'
+
 require File.join(File.dirname(__FILE__), 'azure', 'core', 'auth', 'shared_access_signature')
 require File.join(File.dirname(__FILE__), 'paperclip', 'storage', 'azure')
+
+module Azure
+  module Blob
+    BlobService.class_eval do
+      def initialize(signer=Core::Auth::SharedKey.new, account_name=Azure.config.storage_account_name)
+        super(signer, account_name)
+        @host = "http://#{account_name}.blob.core.windows.net"
+      end
+    end
+  end
+end
