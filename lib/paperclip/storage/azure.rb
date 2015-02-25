a084dbdd3e24b45e4d4e2a998e9217a2c013c10f
Jordan Yaker <jordan.e.yaker@gmail.com>
2015-02-25 13:50:13 -0500

    Fixed the retry policy to check if the request was actually successful.

--- lib/paperclip/storage/azure.rb
+++ lib/paperclip/storage/azure.rb
@@ -140,7 +140,8 @@ module Paperclip
             status_code = 500 if status_code == 0 
             retry_data[:count] ||= 0
             
-            if (status_code >= 300 && status_code < 500 && status_code != 408) ||
+            if  (!response.nil? && response.success? && retry_data[:error].nil?) ||
+                (status_code >= 300 && status_code < 500 && status_code != 408) ||
                 status_code == 501 ||
                 status_code == 505 ||
                 (!retry_data[:error].nil? && retry_data[:error].description == 'Blob type of the blob reference doesn\'t match blob type of the blob.') ||
